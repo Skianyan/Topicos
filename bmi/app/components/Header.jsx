3db236c7a197cddcfdcf54ac38026c5abbec94f4
Skianyan <99053383+Skianyan@users.noreply.github.com>
2023-09-21 23:33:05 -0700

    lol

--- bmi/app/components/Header.jsx
+++ bmi/app/components/Header.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-
+import logo from "./logo.svg";
 const Header = () => {
 	return (
-		<header className="flex p-3 bg-slate-700 text-white justify-evenly">
-			<h1>LOGO</h1>
-			<nav className="flex list-none">
+		<header className="flex p-3 bg-emerald-950 text-white justify-evenly">
+			<img src="https://upload.wikimedia.org/wikipedia/commons/f/f8/Logo_del_Gobierno_de_M%C3%A9xico_%282018%29.png" alt="logo"
+			className="w-20" />
+			<nav className="flex list-none space-x-5 font-sans font-thin">
 				<li>Registro de vacunacion</li>
 				<li>Informacion sobre COVID-19</li>
 				<li>Tramites</li>
 				<li>Gobierno</li>
+				<li><img src="search.png"/></li>
 			</nav>
 		</header>
 	);
--- bmi/app/page.js
+++ bmi/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1>Holis</h1>
+
     </main>
   );
 }
